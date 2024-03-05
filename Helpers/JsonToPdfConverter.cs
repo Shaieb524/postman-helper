16a78817bf8c5da98f454702ed5ae7a93ef1de95
Joud Shaieb <joudshaieb524@gmail.com>
2024-03-06 02:37:46 +0400

    cln

--- Helpers/JsonToPdfConverter.cs
+++ Helpers/JsonToPdfConverter.cs
@@ -46,10 +46,6 @@ namespace postman_helper.Helpers
         private static void ProcessJsonItem(JToken item, Document document)
         {
             var type = item.Type.ToString();
-            var tt = item.Children();
-            var t3 = item.Values();
-            var t4 = item.Count();
-
             int? itemChildsCount = 0;
 
             if (item is JObject)
@@ -59,24 +55,10 @@ namespace postman_helper.Helpers
 
             switch (type)
             {
-                //case "Property":
-                //    if (item["item"] != null)
-                //    {
-                //        document.Add(new Paragraph($"Request: {item["name"]?.ToString()}"));
-                //    }
-                //    break;
-
                 case "Array":
                     if (item.Next is null)
                     {
-                        for (int i = 0; i < 10; i++)
-                        {
-                            var ttt = item[i];
-                            if (ttt != null)
-                            {
-                                ProcessJsonItem(item[i], document);
-                            }
-                        }
+                        ProcessJsonItem(item[i], document);
                     }
                     break;
 
@@ -96,26 +78,6 @@ namespace postman_helper.Helpers
                     Console.WriteLine("DDDDDd");
                     break;
             }
-
-            /*
-			if (!(item is JArray))
-			{
-                if (item["item"] != null)
-                {
-                    document.Add(new Paragraph($"Folder: {item["name"]?.ToString()}"));
-                    ProcessJsonItem(item["item"], document);
-                }
-            }
-			else
-			{
-                // Item name
-                string itemName = item[0]["name"]?.ToString();
-                if (!string.IsNullOrEmpty(itemName))
-                {
-                    document.Add(new Paragraph($"Request: {itemName}"));
-                }
-            }
-			*/
         }
     }
 }
